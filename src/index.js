6f471f5bc3034bd1df88236f94b1a7af4d9809fb
Nhat <nhatld1410@gmail.com>
2024-07-14 07:05:15 +0700

    add-routing-and-controller

--- src/index.js
+++ src/index.js
@@ -6,23 +6,28 @@ const { execPath } = require('process');
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname,'public')));
 
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
 //HTTP logger
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 //Template engine
 app.engine('hbs', handlebars.engine({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
+//Route init
+route(app);
 
-app.get('/',(req,res) => {
-    res.render('home');
-});
 
-app.get('/news',(req,res) => {
-    res.render('news');
-});
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
