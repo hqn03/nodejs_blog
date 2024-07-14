059896f73fe812e0786af9e8fbfe1820f753a22b
Nhat <nhatld1410@gmail.com>
2024-07-14 21:02:02 +0700

    add prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -8,26 +8,26 @@ const port = 3000;
 
 const route = require('./routes');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
 app.use(express.json());
 
 //HTTP logger
 // app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', handlebars.engine({extname: '.hbs'}));
+app.engine('hbs', handlebars.engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 //Route init
 route(app);
 
-
-
-
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () =>
+    console.log(`Example app listening at http://localhost:${port}`),
+);
--- src/routes/news.js
+++ src/routes/news.js
@@ -6,8 +6,6 @@ const newsController = require('../app/controllers/NewsController');
 // newsController.index
 
 route.use('/:slug', newsController.show);
-                route.use('/', newsController.index);
-
-
+route.use('/', newsController.index);
 
 module.exports = route;
